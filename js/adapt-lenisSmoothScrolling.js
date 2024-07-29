0e02b98352ab6177d6a354c78239f5271f4a92e1
Ignacio Cinalli <nachocinalli@gmail.com>
2024-07-29 15:30:16 -0300

    Modify code for compatibility with v5.7.0 and earlier versions

--- js/adapt-lenisSmoothScrolling.js
+++ js/adapt-lenisSmoothScrolling.js
@@ -1,71 +1,65 @@
-import Adapt from 'core/js/adapt';
-import Lenis from 'libraries/lenis';
-
-class LenisSmoothScrolling extends Backbone.Controller {
-  initialize() {
-    this.listenToOnce(Adapt, 'router:location', this.onAdaptInitialize);
-    this.listenTo(Adapt, {
-      'notify:opened': this.onNotifyOpened,
-      'notify:closed': this.onNotifyClosed
-    });
-    this.handleScroll();
-  }
-
-  static get courseConfig() {
-    return Adapt.course.get('_lenisSmoothScrolling');
-  }
-
-  onAdaptInitialize() {
-    if (!LenisSmoothScrolling.courseConfig || !LenisSmoothScrolling.courseConfig._isEnabled) {
-      return;
-    }
-    this.handleScroll();
-    if (Adapt.GSAP && Adapt.GSAP.isEnabled()) {
-      return this.setupGsap();
-    }
-
-    this.setup();
-  }
-
-  setupGsap() {
-    this.lenis = new Lenis();
-    this.lenis.on('scroll', (e) => {
+define(['core/js/adapt', 'libraries/lenis'], function (Adapt, Lenis) {
+  var LenisSmoothScrolling = Backbone.Controller.extend({
+    initialize: function () {
+      this.listenToOnce(Adapt, 'router:location', this.onAdaptInitialize);
+      this.listenTo(Adapt, {
+        'notify:opened': this.onNotifyOpened,
+        'notify:closed': this.onNotifyClosed
+      });
       this.handleScroll();
-      Adapt.GSAP.ScrollTrigger.update();
-    });
-    const self = this;
-    Adapt.GSAP.lib.ticker.add((time) => {
-      self.lenis.raf(time * 1000);
-    });
+    },
 
-    Adapt.GSAP.lib.ticker.lagSmoothing(0);
-  }
+    onAdaptInitialize: function () {
+      if (!Adapt.course.get('_lenisSmoothScrolling') || !Adapt.course.get('_lenisSmoothScrolling')._isEnabled) {
+        return;
+      }
+      this.handleScroll();
+      if (Adapt.GSAP && Adapt.GSAP.isEnabled()) {
+        return this.setupGsap();
+      }
 
-  setup() {
-    this.lenis = new Lenis();
-    this.lenis.on('scroll', this.handleScroll);
-    const self = this;
-    function raf(time) {
-      self.lenis.raf(time);
+      this.setup();
+    },
+
+    setupGsap: function () {
+      this.lenis = new Lenis();
+      this.lenis.on('scroll', (e) => {
+        this.handleScroll();
+        Adapt.GSAP.ScrollTrigger.update();
+      });
+      const self = this;
+      Adapt.GSAP.lib.ticker.add((time) => {
+        self.lenis.raf(time * 1000);
+      });
+
+      Adapt.GSAP.lib.ticker.lagSmoothing(0);
+    },
+
+    setup: function () {
+      this.lenis = new Lenis();
+      this.lenis.on('scroll', this.handleScroll);
+      const self = this;
+      function raf(time) {
+        self.lenis.raf(time);
+        requestAnimationFrame(raf);
+      }
+
       requestAnimationFrame(raf);
+    },
+
+    handleScroll: function () {
+      const scrollTop = document.documentElement.scrollTop;
+      Adapt.trigger('lenis:scroll', scrollTop);
+    },
+
+    onNotifyOpened: function (view) {
+      view.$el.attr('data-lenis-prevent', '');
+      this.lenis.stop();
+    },
+
+    onNotifyClosed: function () {
+      this.lenis.start();
     }
-
-    requestAnimationFrame(raf);
-  }
-
-  handleScroll() {
-    const scrollTop = document.documentElement.scrollTop;
-    Adapt.trigger('lenis:scroll', scrollTop);
-  }
-
-  onNotifyOpened(view) {
-    view.$el.attr('data-lenis-prevent', '');
-    this.lenis.stop();
-  }
-
-  onNotifyClosed() {
-    this.lenis.start();
-  }
-}
-
-export default new LenisSmoothScrolling();
+  });
+  return new LenisSmoothScrolling();
+});
